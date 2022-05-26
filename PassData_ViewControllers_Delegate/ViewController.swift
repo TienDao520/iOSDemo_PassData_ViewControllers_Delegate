0bf84b7ed6ee345921faabccd3bd31cf5a11330a
Tien Dao <tiendao5240@gmail.com>
2022-05-26 12:05:42 -0400

    Added SecondViewController

--- PassData_ViewControllers_Delegate/ViewController.swift
+++ PassData_ViewControllers_Delegate/ViewController.swift
@@ -13,15 +13,36 @@ protocol SendDataDelegate {
 
 class FirstViewController: UIViewController, SendDataDelegate {
     func sendMessage(text: String) {
-        
+        print(text)
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //passing data automatically
+        perform(#selector(advance), with: nil, afterDelay: 2)
     }
 
+    @objc func advance() {
+        let vc2 = SecondViewController()
+        vc2.delegate = self
+        vc2.sentTextToSecondVC = "sent Text ToSecond VC"
+        present(vc2, animated: true, completion: nil)
+    }
 
 }
 
+
+class SecondViewController: UIViewController {
+    var delegate:SendDataDelegate?
+    var sentTextToSecondVC:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        print(sentTextToSecondVC)
+        
+    }
+    
+    
+}
